1e98877ce71e1b4b9a5700ed08774e733572c6cf
tngusqkr1 <tngusqkr1@gmail.com>
2019-05-14 21:34:50 +0900

    added login

--- code/JikByeon/urls.py
+++ code/JikByeon/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 from app import views
+from django.urls import path, re_path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name = "home"),
+    re_path('^$', views.home, name="home"),
+    re_path('^login/$', views.login, name="login"),
 ]
