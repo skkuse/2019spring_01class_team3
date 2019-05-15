cab89b83a46cfecd4e9ccb080aa55976644d38c0
tngusqkr1 <tngusqkr1@gmail.com>
2019-05-15 16:13:42 +0900

    added register func

--- code/app/models.py
+++ code/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    password = models.TextField()
+    phone_num = models.TextField()
+
+class Country(models.Model):
+    cid = models.IntegerField(primary_key=True)
+    cname = models.TextField()
+
+
+class Product(models.Model):
+    pid = models.IntegerField(primary_key=True)
+    pcode = models.TextField(null=False)
+    brand = models.TextField()
+    pname = models.TextField()
+    category = models.TextField()
+    price = models.IntegerField()
+    url = models.URLField()
+    cid = models.ForeignKey(Country, on_delete = models.CASCADE)
---
+++ code/app/urls.py
@@ -0,0 +1,9 @@
+from app import views
+from django.urls import path, re_path
+from django.contrib import admin
+
+
+urlpatterns = [
+    re_path('^admin/', admin.site.urls),
+    re_path('^login/$', views.login, name="login"),
+]
