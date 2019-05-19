62f910d01d11bb29581e91ab0652c6931445e025
saanmin <kimsun94@icloud.com>
2019-05-19 12:25:41 +0900

    del&site button added

--- code/app/views.py
+++ code/app/views.py
@@ -10,7 +10,6 @@ from .models import *
 
 def home(request):
     products = Product.objects.all()
-
     return render(request, 'index.html', {'products':products})
 
 
@@ -26,7 +25,6 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, 'Account created for %s'%username)
             return redirect('home')
-
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
@@ -34,3 +32,15 @@ def register(request):
 def view_favorites(request) :
     favorites = Favorite.objects.filter(uid=100)
     return render(request, 'favorites.html',{'favorites':favorites})
+
+'''
+def del_favorites(request) :
+    #html에서 삭제하고자 하는 del_fid 전달받아와서 연결해놓으면 해결
+    del_f = Favorite.objects.get(fid = del_fid)
+    del_f.delete()
+
+    #삭제한 뒤에 위의 view_favorites 메서드를 재수행 하면 업데이트된 My favorites로 바로 이동할 수 있을 것 같음
+    return render(request, favorites)
+'''
+
+
