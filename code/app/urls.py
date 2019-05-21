a195a842299fe2edda054d97ed851360a43472cf
HYEN SOO JANG <janghs1993@gmail.com>
2019-05-21 15:06:42 +0900

    third trial

--- code/app/urls.py
+++ code/app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     re_path('^admin/', admin.site.urls),
-    re_path('^login/$', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    re_path('^login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     re_path('^logout/$', auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     re_path('^detail/(?P<pcode>.*)$', views.detail, name="detail"),
     re_path('^searchList/$', views.searchList, name="searchList"),
