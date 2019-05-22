8a06b7b7a2ac16925e1b317e050effd13a5b7ccd
dlrydud99 <dlrydud99@gmail.com>
2019-05-22 00:27:01 -0700

    dd

--- code/app/models.py
+++ code/app/models.py
@@ -7,13 +7,13 @@ class Country(models.Model):
     cid = models.IntegerField(primary_key=True)
     cname = models.TextField()
     short_cname = models.TextField()
+    currency = models.TextField()
 
     def __str__(self):
-        return self.cname
+        return str(self.cid)
 
 
 class Product(models.Model):
-    pid = models.IntegerField(primary_key=True)
     pcode = models.TextField(null=False)
     brand = models.TextField()
     pname = models.TextField()
@@ -23,12 +23,12 @@ class Product(models.Model):
     cid = models.ForeignKey(Country, on_delete = models.CASCADE)
 
     def __str__(self):
-        return str(self.pid) + ", " + self.pname + ", " + str(self.cid)
-
+        return str(self.id) + ", " + self.pname + ", " + str(self.cid)
 
 class Favorite (models.Model) :
     fid = models.IntegerField(primary_key=True)
     pid = models.ForeignKey(Product, on_delete = models.CASCADE)
+    kprice = models.TextField()
     #uid = models.IntegerField()
 
     #USER 연동 성공하면...
@@ -39,5 +39,3 @@ class Favorite (models.Model) :
 
     def __str__(self) :
         return str(self.fid) + ", "+ str(self.pid)
-
-
--- code/app/views.py
+++ code/app/views.py
@@ -5,32 +5,73 @@ from .models import *
 import requests as req
 from datetime import datetime
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.db.models.query import QuerySet
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models.query import QuerySet
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.csrf import csrf_protect
 
+# 기본 함수
+
 
 def home(request):
-    distinct_products = Product.objects.values('pcode').distinct()
+    ko_products = Product.objects.filter(cid=1)
+
     products = []
 
-    for p in distinct_products:
-        products.append(Product.objects.filter(pcode=p['pcode'])[0])
+    count = 0
+    for p in ko_products:
+        if count == 20:
+            break
+        count += 1
+        products.append(p)
 
     return render(request, 'index.html', {'products': products})
 
+
+def getExRate():
+    # day format : yyyymmdd
+    day = str(datetime.today().year) + \
+        '%02d' % datetime.today().month + str(datetime.today().day)
+
+    url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=EgAaHKyguuwATPc8pOp29tLMJNOSYRw8&searchdate=%s&data=AP01' % day
+
+    ex_rate_response = req.get(url)
+    ex_rate_json = ex_rate_response.json()
+
+    if str(ex_rate_response) != '<Response [200]>' or len(ex_rate_json) == 0:
+        day = str(datetime.today().year) + \
+            '%02d' % datetime.today().month + str(datetime.today().day - 2)
+
+        url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=EgAaHKyguuwATPc8pOp29tLMJNOSYRw8&searchdate=%s&data=AP01' % day
+
+    ex_rate_response = req.get(url)
+    ex_rate_json = ex_rate_response.json()
+
+    ex_rate = {}
+
+    # DEAL_BAS_R
+    for ex in ex_rate_json:
+        if ex['cur_unit'] == 'USD':
+            ex_rate['미국'] = float(ex['deal_bas_r'].replace(',', ''))
+        elif ex['cur_unit'] == 'EUR':
+            ex_rate['프랑스'] = float(ex['deal_bas_r'].replace(',', ''))
+        elif ex['cur_unit'] == 'JPY(100)':
+            ex_rate['일본'] = float(ex['deal_bas_r'].replace(',', '')) / 100
+
+    return ex_rate
+
+# User Management System
 @csrf_protect
-def SignIn(request):
+def login_request(request):
     if request.method == "POST":
         input_email = request.POST.getlist('email')
         input_password = request.POST.getlist('password')
         print(input_email)
         print(input_password)
-        user = authenticate(email=input_email, password = input_password)
-        #user.is_active = True
+        user = authenticate(email=input_email, password=input_password)
+        # user.is_active = True
 
         if user is not None:
             print("not none")
@@ -42,9 +83,10 @@ def SignIn(request):
     else:
         return render(request, 'login.html')
 
-def SignOut(request):
+
+def logout_request(request):
     logout(request)
-    return render(request, 'index.html')
+    return render(request, 'logout.html')
     # if request.method == 'POST':
     #     return redirect('home')
     # return render(request, 'logout.html')
@@ -64,43 +106,70 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 
+# Favorites System
+def view_favorites(request):
+    # user login 실패시..
+    # favorites = Favorite.objects.filter(uid = 100)
+
+        # user 성공하면...
+    favorites = Favorite.objects.filter(uid=request.user)
+
+    # API 추가
+    ex_rate = getExRate()
+    for f in favorites:
+        if int(f.id.cid) != 1:
+            f.id.price = "{:,}".format(int(ex_rate[str(f.id.cid.cname)] * int(f.id.price)))
+
+
+    return render(request, 'favorites.html', {'favorites': favorites})
+
+
+def delFavorite(request, del_fid):
+    if request.method == 'GET':
+        del_favorite = Favorite.objects.get(fid=del_fid)
+        del_favorite.delete()
+        return view_favorites(request)
+
+
+def addFavorite(request, add_id):
+    if request.method == 'GET':
+        '''
+        product = Product.objects.get(id = add_id)
+        favorite = Favorite(id=product, uid=100)
+        favorite.save()
+        return view_favorites(request)
+        '''
+
+        # User 연동 성공하면...
+
+        user = request.user
+        product = Product.objects.get(id=add_id)
+        ko_id = Product.objects.filter(pcode=product.pcode) #.filter(cid=0)
+        kprice = '0'
+        for k in ko_id:
+            if k.cid == 1: kprice = "{:,}".format(k.price)
+
+        favorite = Favorite(id=product, uid=user, kprice=kprice)
+        favorite.save()
+        return detail(request, product.pcode)
+
+        # 원하는 페이지로 설정...보통 즐겨찾기는 세부페이지에서 진행하므로
+
+# Comparing System
+
+
 def detail(request, pcode):
     if request.method == 'GET':
         products = Product.objects.filter(pcode=pcode)
 
-        day = str(datetime.today().year) + '%02d'%datetime.today().month + str(datetime.today().day)
-
-        url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=EgAaHKyguuwATPc8pOp29tLMJNOSYRw8&searchdate=%s&data=AP01'%day
-
-        ex_rate_response = req.get(url)
-        ex_rate_json = ex_rate_response.json()
-
-        if str(ex_rate_response) != '<Response [200]>' or len(ex_rate_json) == 0:
-            day = str(datetime.today().year) + '%02d'%datetime.today().month + str(datetime.today().day - 2)
-
-            url = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=EgAaHKyguuwATPc8pOp29tLMJNOSYRw8&searchdate=%s&data=AP01'%day
-
-            ex_rate_response = req.get(url)
-            ex_rate_json = ex_rate_response.json()
-
-
-        ex_rate = {}
-
-        # DEAL_BAS_R
-        for ex in ex_rate_json:
-            if ex['cur_unit'] == 'USD':
-                ex_rate['미국'] = float(ex['deal_bas_r'].replace(',',''))
-            elif ex['cur_unit'] == 'EUR':
-                ex_rate['프랑스'] = float(ex['deal_bas_r'].replace(',',''))
-            elif ex['cur_unit'] == 'JPY(100)':
-                ex_rate['일본'] = float(ex['deal_bas_r'].replace(',','')) / 100
-
         p = products[0]
 
+        ex_rate = getExRate()
 
         for product in products:
-            if str(product.cid) != '대한민국':
-                product.price = int(int(product.price) * ex_rate[str(product.cid)])
+            if int(str(product.cid)) != 1:
+                product.price = int(int(product.price) *
+                                    ex_rate[str(product.cid.cname)])
             product.price = "{:,}".format(product.price)
 
         return render(request, 'product_detail.html', {'products': products, 'p': p})
