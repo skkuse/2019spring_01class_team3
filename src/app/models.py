671a2630887d14d1574f5fcb228f17a9d16e0a97
saanmin <kimsun94@icloud.com>
2019-06-04 13:13:48 +0900

    Final Favorite

--- src/app/models.py
+++ src/app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Country(models.Model):
--- src/app/urls.py
+++ src/app/urls.py
@@ -11,10 +11,9 @@ urlpatterns = [
             auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     re_path('^detail/(?P<pcode>.*)$', views.detail, name="detail"),
     re_path('^searchList/', views.searchList, name="searchList"),
-    # $ : 뒤에 더이상 다른 슬래시가 오지 않도록 막아버리는 기능 (정규식이에요)
-    re_path('^favorites/(?P<del_fid>.*)$',
-            views.delFavorite, name="delFavorite"),
-    re_path('^product_detail/(?P<add_id>.*)$',
-            views.addFavorite, name="addFavorite"),
+
+    re_path('^delFavorite/(?P<del_fid>.*)$', views.delFavorite, name="delFavorite"),
+    re_path('^product_like/$', views.product_like, name='product_like'),
+
     re_path('^filter/(?P<f>.*)/(?P<name>.*)$', views.home_filter, name='home_filter'),
 ]
